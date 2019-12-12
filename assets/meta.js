763ac29b2dcd6a526729bee9d27704704925a09f
Jung <iveinvalue@gmail.com>
2019-12-12 13:01:59 +0900

    update

--- assets/meta.js
+++ assets/meta.js
@@ -16,6 +16,25 @@ window.addEventListener('load', function () {
     // Now you can start your app & access web3 freely:
     startApp();
 });
+
+var getParameter = function (param) {
+    var returnValue;
+    var url = location.href;
+    var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&');
+    for (var i = 0; i < parameters.length; i++) {
+        var varName = parameters[i].split('=')[0];
+        if (varName.toUpperCase() == param.toUpperCase()) {
+            returnValue = parameters[i].split('=')[1];
+            return decodeURIComponent(returnValue);
+        }
+    }
+};
+
+function setAddresss(){
+    //alert(getParameter('id'));
+    document.getElementById('myaddd').value = getParameter('id');
+}
+
 function startApp() {
     // Modern DApp Browsers
     if (window.ethereum) {
